4f66cf4741f4ac34821c57325db4c3aa8cfa9c06
JAGADEEP S <jagasiva1999@gmail.com>
2025-05-12 10:18:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: React.Children.only expected to receive a single React element child.. Error source: src/components/ui/form.tsx (113:5) @ FormControl
    >
    >   111 |
    >   112 |   return (
    > > 113 |     <Slot // Slot will use props.children. If props.asChild is true, it will merge.
    >       |     ^
    >   114 |       ref={ref}
    >   115 |       id={formItemId}
    >   116 |       aria-describedby={
    >
    > Call Stack
    > 10
    >
    > Show 5 ignore-listed frame(s)
    > FormControl
    > src/components/ui/form.tsx (113:5)
    > Object.render
    > src/components/profile/advanced-settings-form.tsx (110:21)
    > FormField
    > src/components/ui/form.tsx (39:7)
    > AdvancedSettingsForm
    > src/components/profile/advanced-settings-form.tsx (103:13)
    > ProfilePage
    > src/app/(app)/profile/page.tsx (51:7)

--- src/components/profile/advanced-settings-form.tsx
+++ src/components/profile/advanced-settings-form.tsx
@@ -107,7 +107,7 @@ export function AdvancedSettingsForm({ user, setUser, anyLoading, setAnyLoading 
                 <FormItem>
                   <FormLabel>Interface Density</FormLabel>
                   <Select onValueChange={field.onChange} value={field.value} disabled={isLoading || anyLoading}>
-                    <FormControl> {/* Removed asChild prop */}
+                    <FormControl asChild> 
                       <SelectTrigger>
                         <SelectValue placeholder="Select interface density" />
                       </SelectTrigger>
